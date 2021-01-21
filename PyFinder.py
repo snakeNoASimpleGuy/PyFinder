1efbc581bc7bbec0bd338d91702157731ce3bb64
J4SH185 <72822908+J4SH185@users.noreply.github.com>
2021-01-21 12:33:19 -0500

    Update PyFinder.py
    Agregue un buscador de Login de administrador en esta version

--- PyFinder.py
+++ PyFinder.py
@@ -6,42 +6,81 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t','--target',help="Indica el dominio de la victima")
-parser = parser.parse_args()
 
-def menu():
-    print("PyFinder\nSi me falta un banner pero pa despues\n\n#Hecho por J4SH")
-menu()
+parser.add_argument('-t','--target',help="Indica el dominio victima")
+parser.add_argument('-s','--subdominios',action='store_true',help="Encuentra subdominios")
+parser.add_argument('-a','--admin',action='store_true',help="Encuentra la el login de admin")
+
+args = parser.parse_args()
+
+def banner():
+    print("______       ______  _             _             ")
+    print("| ___ \      |  ___|(_)           | |            ")
+    print("| |_/ /_   _ | |_    _  _ __    __| |  ___  _ __ ")
+    print("|  __/| | | ||  _|  | || '_ \  / _` | / _ \| '__|")
+    print("| |   | |_| || |    | || | | || (_| ||  __/| |   ")
+    print("\_|    \__, |\_|    |_||_| |_| \__,_| \___||_|   ")
+    print("        __/ |                                    ")
+    print("       |___/                                     ")
 
 def main():
-    if parser.target:
-        if path.exists('subdominios.txt'): #el subdominios.txt puedes cambiarlo por cualquier otro diccionario en github hay mas :)
+    if args.target:
+        print(f"\n\nLa pagina es:  {args.target}\n ")
+
+    if args.admin:
+        if path.exists('admin.txt'):
+            wordlist = open('admin.txt','r')
+            wordlist = wordlist.read().split('\n')
+
+            for admin in wordlist:
+                url = "http://"+admin+"."+args.target
+                try:
+                    requests.get(url)
+                except requests.ConnectionError:
+                    pass
+                else:
+                    print("[+] Admin login Descubierto: " + url)
+
+            for admin in wordlist:
+                url = "https://" + admin + "." + args.target
+                try:
+                    requests.get(url)
+                except requests.ConnectionError:
+                    pass
+                else:
+                    print("[+] Admin login Descubierto: " + url)
+
+    if args.subdominios:
+        if path.exists('subdominios.txt'):
             wordlist = open('subdominios.txt','r')
             wordlist = wordlist.read().split('\n')
 
             for subdominio in wordlist:
-                url = "http://"+subdominio+"."+parser.target
+                url = "http://"+subdominio+"."+args.target
                 try:
                     requests.get(url)
                 except requests.ConnectionError:
                     pass
                 else:
-                    print("[*]Subdominio encontrado " + url)
+                    print("[+] Subdominio Descubierto: " + url)
+
             for subdominio in wordlist:
-                url = "https://"+subdominio+"."+parser.target
+                url = "https://" + subdominio + "." + args.target
                 try:
                     requests.get(url)
                 except requests.ConnectionError:
                     pass
                 else:
-                    print("[*]Subdominio encontrado " + url)
+                    print("[+] Subdominio Descubierto: " + url)
+
     else:
-        print("Por favor usa un subdominio valido")
+        print("\n\nPor favor usa '-h' para ver las opciones")
         sys.exit()
 
 if __name__ == '__main__':
     try:
+        banner()
         main()
     except KeyboardInterrupt:
-        print("Saliendo....")
+        print("\nSaliendo....")
         sys.exit()
